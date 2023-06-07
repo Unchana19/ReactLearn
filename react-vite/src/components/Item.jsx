6618c132d15f7a5717ce559b55ff6cfefcdca795
annchana <annchanaoat@gmail.com>
2023-06-08 01:28:51 +0700

    components

---
+++ react-vite/src/components/Item.jsx
@@ -0,0 +1,21 @@
+import "./Item.css";
+import { useState } from 'react'
+
+export default function Item(props) {
+    const {title, description} = props
+    const [show, setShow] = useState(false);
+
+  return (
+    <section>
+      <article>
+        <div className="content">
+          <h4>{title}</h4>
+          <button onClick={()=>setShow(!show)}>
+            {show ? "ซ่อน" : "แสดง"}
+          </button>
+        </div>
+        {show && <p>{description}</p>}
+      </article>
+    </section>
+  );
+}
