f8f2914f5fc6def681b7a4c10179206dfe5fcc25
annchana <annchanaoat@gmail.com>
2023-06-08 21:37:46 +0700

    tailwind

--- form-app/tailwind.config.js
+++ form-app/tailwind.config.js
@@ -4,6 +4,8 @@ export default {
     "./index.html",
     "./src/App.jsx",
     "./src/main.jsx",
+    "./src/components/Form.jsx",
+    "./src/components/ItemForm.jsx",
   ],
   theme: {
     extend: {},
