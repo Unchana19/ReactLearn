6ed5564568ceba26d7df5467a625220322f63646
Unchana19 <annchanaoat@gmail.com>
2023-06-15 18:22:49 +0700

    main

--- dark-light/src/App.jsx
+++ dark-light/src/App.jsx
@@ -1,13 +1,20 @@
+import Content from "./components/Content"
+import Title from "./components/Title"
+import { createContext, useState } from "react"
+import "../src/App.css";
 
+export const ThemeContext = createContext()
 
 function App() {
+  const [theme, setTheme] = useState('light');
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}} >
+      <div className={`flex flex-col items-center justify-center h-screen ease-in duration-200 ${theme}`}>
+          <Title />
+          <Content />            
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
