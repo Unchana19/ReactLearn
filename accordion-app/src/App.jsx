d0018329274bca79a6ac97639f26e23eb6056b9f
annchana <annchanaoat@gmail.com>
2023-06-21 15:49:32 +0700

    start project

---
+++ accordion-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import data from './data/data'
+
+function App() {
+  const [content, setContent] = useState(data);
+
+  return (
+    <main>
+      <div>
+          
+      </div>
+    </main>
+  )
+}
+
+export default App
