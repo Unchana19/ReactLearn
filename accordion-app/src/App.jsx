7050417b8df98c9ba8a786f056e85579f012c437
annchana <annchanaoat@gmail.com>
2023-06-21 16:31:06 +0700

    main

--- accordion-app/src/App.jsx
+++ accordion-app/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import data from './data/data'
+import Content from './components/Content';
 
 function App() {
   const [content, setContent] = useState(data);
 
   return (
-    <main>
-      <div>
-          
+    <main className='h-screen bg-gradient-to-t from-slate-900 to-gray-950'>
+      <div className='h-screen flex flex-col w-11/12 items-center justify-center'>
+          <h3 className='pb-20 font-bold text-3xl text-white'>Tool for Developing Website in 2023</h3>
+          <section className='py-5 w-10/12'>
+            {content.map(data => {
+              return <Content key={data.id} {...data} />
+            })}
+          </section>
       </div>
     </main>
   )
