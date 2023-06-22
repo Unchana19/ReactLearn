c51af2aef4f0058fcc3aa14a2b82443f7025b0aa
annchana <annchanaoat@gmail.com>
2023-06-22 18:05:56 +0700

    start project

---
+++ currency-convert/src/App.jsx
@@ -0,0 +1,12 @@
+import money from './assets/money.png'
+
+function App() {
+  return (
+    <div>
+      <img src={money} alt='logo' />
+      <h1>Currency Convert App</h1>
+    </div>
+  )
+}
+
+export default App
