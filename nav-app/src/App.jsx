8fa7e4b46d071cf2ca5729e2f586a2bcaf691ef2
annchana <annchanaoat@gmail.com>
2023-06-23 16:35:42 +0700

    main

--- nav-app/src/App.jsx
+++ nav-app/src/App.jsx
@@ -1,11 +1,23 @@
-
+import Navigation from "./components/Navigation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/page/Home";
+import Member from "./components/page/Member";
+import Product from "./components/page/Product";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
+  const [showMenu, setShowMenu] = useState(false);
 
-    </div>
-  )
+  return (
+    <BrowserRouter>
+      <Navigation showMenu={showMenu} setShowMenu={setShowMenu} />     
+    <Routes>
+      <Route path="/" element={<Home setShowMenu={setShowMenu} />}></Route>
+      <Route path="/member" element={<Member setShowMenu={setShowMenu} />}></Route>
+      <Route path="/product" element={<Product setShowMenu={setShowMenu} />}></Route>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
