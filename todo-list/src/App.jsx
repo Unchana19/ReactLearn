88f9b952438598be66aa800a388b7d6658b153b7
annchana <annchanaoat@gmail.com>
2023-06-25 15:16:10 +0700

    start project

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>TodoList App</h1>
+      <form>
+        <input type="text"></input>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
